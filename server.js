106043c78a3cc5daa1eaae2c9a52e0772c6038e3
cjacques4 <jacquesclement4@gmail.com>
2018-03-16 14:01:01 +0100

    scrapping of the brands and the models

---
+++ server.js
@@ -0,0 +1,13 @@
+const {getBrands} = require('node-car-api');
+const {getModels} = require('node-car-api');
+
+getBrands().then(res=>{
+  console.log(res)
+}).catch(err=>{
+  console.log(err)
+})
+getModels('PEUGEOT').then(res=>{
+  console.log(res)
+}).catch(err=>{
+  console.log(err)
+})
